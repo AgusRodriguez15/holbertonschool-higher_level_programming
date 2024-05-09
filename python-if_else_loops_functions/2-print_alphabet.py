27b3ae0c2125df24ca381c29108f40de9b4ed50b
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-09 17:27:30 +0000

    3-print_alphabet solution

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for alphabet in range(97,123):
+for alphabet in range(97, 123):
     alpha = chr (alphabet)
-    print("{}".format(alpha), end="")
-    
+    print ("{}".format(alpha), end="")
---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for alphabet in range(97, 123):
+    alpha = chr (alphabet)
+    if alphabet != 101:
+        if alphabet != 113:
+            print("{}".format(alpha), end="")
+            
