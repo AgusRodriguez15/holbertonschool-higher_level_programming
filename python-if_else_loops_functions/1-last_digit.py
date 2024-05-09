fdb2773e93fb101f159f7787ea7764e5ff006777
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-09 13:44:20 +0000

    2-print_alphabet.py solution

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-numb
+last_digit = number % 10
+number2 = abs(number)
+#print(number2)
+if last_digit > 5:
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    if number < 0:
+        number2 % 10    
+        print(number2)   
---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+for alphabet in range(97,122):
+    #print(alphabet)
+    alpha = chr (alphabet)
+    print(alpha, end="")
+    
