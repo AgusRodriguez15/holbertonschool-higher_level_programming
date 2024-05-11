07fab702b7bb5cd424aabc7af45bac9906a9f58b
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-11 22:59:36 +0000

    6 print_comb solution

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range (10):
-    for j in range (10):
+    for j in range (i + 1, 10):
         comb = str(i) + str(j)
         if str(i) != str(j):
             print("{}".format(comb), end=", ")
-    else:
-        print("{}".format(comb))
+print("{}".format(comb))
