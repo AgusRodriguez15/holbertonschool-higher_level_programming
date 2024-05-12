1dda84c6fae693d1e657b861428986f4994a2753
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-12 21:53:15 +0000

    1 calculation solution

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from calculator_1 import add
+a = 10
+b = 5
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
