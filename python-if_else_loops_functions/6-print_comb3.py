2153a69d82678068262f689bfc8831d35a5067e3
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-12 00:56:56 +0000

    correct 6_print_comb

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 for i in range (10):
     for j in range (i + 1, 10):
         comb = str(i) + str(j)
-        if str(i) != str(j):
+        if comb != '89':
             print("{}".format(comb), end=", ")
-print("{}".format(comb))
+else:
+    print("{}".format(comb))
