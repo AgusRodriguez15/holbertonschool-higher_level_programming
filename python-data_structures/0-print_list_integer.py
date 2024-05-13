e24cfa33781f94a21f9f9cb4bf5f2888aebed9de
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-13 21:52:34 +0000

    0 print list solution.

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in my_list:
-        salida = "{:.2f}"
-        print(salida.format(i))
+        salida = "{j:.2f}"
+        print(salida.format(j = i))
