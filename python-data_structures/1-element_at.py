29f4fdd2e407f7b3545ca52e1504cb1730b6eb18
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-14 00:03:14 +0000

    1 element solution

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:
-        int(i)
     if idx < 0:
         return None
-    if idx > i:
+    if idx > len(my_list):
         return None
-    while idx == i:
-        print("{:d}".format(my_list[idx]))
+    print("{:d}".format(my_list[idx]))
