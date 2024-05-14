911be5b8c6dccefd1a2a7e23f1a31ef88b22db29
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-14 23:57:08 +0000

    5 no copy solutioon

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = my_string.translate({ord('c'): None})
-
+    new = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new += i 
     return new
