bf41326305ce7a4c69655fdd04a4cd10c116386d
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-15 15:02:28 -0800

    11 delete solution

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new = my_list.copy()
+    for i in new:
+        if new[i] / 2 == 0:
+            return new, True
+        else:
+            return new, False
