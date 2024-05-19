c3ed8f1b183e947efa4ec20ec83c6e65ae621fc5
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-18 18:22:53 -0800

    0 saquare solution

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-class Square:  # Define una clase llamada Square
-    pass  # Sentencia "pass" indica que la clase está vacía por ahora
+class Square:  # Define 
+    pass  # Sentencia 
