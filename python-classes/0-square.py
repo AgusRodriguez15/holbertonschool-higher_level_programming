b2dfc1dd35fb6ad5ed8e7d2ecf4628739cbdbdba
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-18 17:58:31 -0800

    0 square solution

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,4 @@
-#!/usr/bin/python3
-class Square:
-    pass
+#!/usr/bin/python3  # Shebang que especifica el intérprete de Python a utilizar
+
+class Square:  # Define una clase llamada Square
+    pass  # Sentencia "pass" indica que la clase está vacía por ahora
