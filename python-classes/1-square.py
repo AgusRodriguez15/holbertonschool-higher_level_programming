e9dc4a17dbe8e1c66c9c7b3befa93ec553e72131
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-19 09:58:10 -0800

    1 - square solution

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Clase que define un cuadrado.
+
+    Atributos privados de instancia:
+        __size: tamaño del cuadrado
+
+    Métodos:
+        __init__(self, size): Constructor.
+    """
+
+    def __init__(self, size):
+        """
+        Constructor de la clase Square.
+
+        Parámetros:
+            size (int): Tamaño del cuadrado.
+        """
+        self.__size = size
