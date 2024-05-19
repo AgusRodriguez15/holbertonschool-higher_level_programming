15beaa97c67d21d2f25f75a0101b6c544fa840d0
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-19 12:48:18 -0800

    3-square solution

---
+++ python-classes/3-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+squareeeee
+"""
+
+
+class Square:
+    """
+    Clase que define un cuadrado.
+
+    Atributos privados de instancia:
+        __size: tamaño del cuadrado
+
+    Métodos:
+        __init__(self, size): Constructor.
+        area(self): Calcula el área del cuadrado.
+    """
+
+    def __init__(self, size=0):
+        """
+        Constructor de la clase Square.
+
+        Parámetros:
+            size (int): Tamaño del cuadrado.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calcula el área del cuadrado.
+
+        Retorna:
+            int: Área del cuadrado.
+        """
+        area = self.__size * self.__size
+        return area
