916f550c071752c375952a9074772d012b35353c
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-18 18:19:30 -0800

    correct 3 safe print

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,6 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Inside result:", None)
         result = None
     finally:
         print("Inside result:", "{}".format(result))
