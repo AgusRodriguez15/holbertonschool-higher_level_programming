03fbdb3d395e60762c13168bc9b0d67afbb91fb4
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-21 11:58:48 -0800

    pycode

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,9 +2,10 @@
 
 """"3 solution"""
 
-def say_my_name(first_name, last_name=""):             
+
+def say_my_name(first_name, last_name=""):
     """
-    Imprime el nombre completo con el formato "My name is <first_name> <last_name>$".
+    Imprime el nombre completo ".
 
     Parámetros:
     first_name (str): El primer nombre.
@@ -15,6 +16,5 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
+
     print("My name is {} {}".format(first_name, last_name))
-    
