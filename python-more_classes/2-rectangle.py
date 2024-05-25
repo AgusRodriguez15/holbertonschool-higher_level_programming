38c05f3bdde625c3ae8f2e1ed848ec1f5bf9fa54
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-25 10:12:04 -0800

    2 rectangle solution

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+"""
+more clases
+"""
+
+
+class Rectangle:  # Clase Rectangle
+    """
+    clases
+    """
+    def __init__(self, width=0, height=0):
+        """def init """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """retrieve it"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set it"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieve it"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set it"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """calculate the area of the rectangle"""
+        area = self.__height * self.width
+        return area
+
+    def perimeter(self):
+        """calculate the perimeter of the rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return "perimeter is equal to 0"
+        permiter = self.__height + self.__width + self.__width + self.__height
+        return permiter
