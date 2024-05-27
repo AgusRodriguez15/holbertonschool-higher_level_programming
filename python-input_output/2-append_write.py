14353eb7c072c63aebfbdfea3888563bbde20488
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-27 14:01:55 -0800

    2 append correction

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,4 +6,4 @@ def append_write(filename="", text=""):
     """add a string to the final"""
     with open(filename, 'w', encoding="UTF-8") as f:
         f.write(text)
-        return f.write(text)
+        return len(text)
