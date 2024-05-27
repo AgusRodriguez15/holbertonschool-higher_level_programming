7f5581a83519411f860659ad116b4dae3e5662b4
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-27 13:56:54 -0800

    documentattion

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,7 @@
 
 
 def append_write(filename="", text=""):
+    """add a string to the final"""
     with open(filename, 'w', encoding="UTF-8") as f:
         f.write(text)
         return f.write(text)
