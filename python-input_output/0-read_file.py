1017a331449b1063a3099cf36459b31f1c2b1c0f
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-27 10:37:33 -0800

    0 read file solution

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""read the file"""
+
+
 def read_file(filename=""):
     """read the file"""
     with open(filename, 'r', encoding="UTF-8") as f:
-      print(f.read(), end='')
+        print(f.read(), end='')
