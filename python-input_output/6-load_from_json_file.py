ffb257f6240c2e1c084ca265ea89346350885734
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-28 09:58:38 -0500

    sss

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,6 +6,4 @@ import json
 def load_from_json_file(filename):
     """returns the JSON"""
     with open(filename, 'w', encoding="UTF-8") as fil:
-        x = fil.read()
-        load = json.loads(x)
-    return load
+        return json.load(fil)
