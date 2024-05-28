6d1b2a9345d7d8debf8ced5f0a05fed9718239df
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-28 09:59:52 -0500

    6 solution

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """returns the JSON"""
-    with open(filename, 'w', encoding="UTF-8") as fil:
+    with open(filename, encoding="UTF-8") as fil:
         return json.load(fil)
