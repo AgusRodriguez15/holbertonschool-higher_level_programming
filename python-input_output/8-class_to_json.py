db125ff733507d400036fc2a94db709d5f53cf3c
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-29 12:39:04 -0500

    7 pycode

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """script that adds all arguments to a Python list, then save them to a file"""
+
+
 def class_to_json(obj):
     dictionary = {}
     dictionary = obj.__dict__
-    return dictionary   
+    return dictionary
