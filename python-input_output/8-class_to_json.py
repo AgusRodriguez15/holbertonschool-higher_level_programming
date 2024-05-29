591682442d4803a1895b53a62aaca2439913cbb5
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-29 12:26:56 -0500

    8 solution

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,4 +2,7 @@
 """script that adds all arguments to a Python list, then save them to a file"""
 import json
 def class_to_json(obj):
-    json.dumps(obj)
+    
+    # Pasar todos los atributos del objeto a un diccionario
+    # "inspeccionar" obj y poner todo lo que tiene adentro de dictionary
+    return  obj.__dict__
