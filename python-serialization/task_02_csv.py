ed0a03ccf7d5c2b5e57ad7f53e97c3dba3d4ebf6
AgusRodriguez15 <8670@holbertonstudents.com>
2024-06-02 08:13:27 -0800

    pep8  correction

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -4,17 +4,16 @@ import json
 
 """Convert csv to json"""
 
+
 def convert_csv_to_json(csv_filename):
-        """Convert csv to json"""
-        with open(csv_filename, 'r', newline='') as f:
-            csv_reader = csv.DictReader(f)
-            data = list(csv_reader)
+    """Convert csv to json"""
+    with open(csv_filename, 'r', newline='') as f:
+        csv_reader = csv.DictReader(f)
+        data = list(csv_reader)
 
-        
-        json_data = json.dumps(data, indent=4)
+    json_data = json.dumps(data, indent=4)
 
-        
-        with open('data.json', 'w') as f_json:
-            f_json.write(json_data)
+    with open('data.json', 'w') as f_json:
+        f_json.write(json_data)
 
-        return True
+    return True
