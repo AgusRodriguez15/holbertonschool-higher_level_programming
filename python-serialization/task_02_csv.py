dc4e6621121736708b4a1f47442be9a92832fadb
AgusRodriguez15 <8670@holbertonstudents.com>
2024-06-02 08:11:11 -0800

    task 02 slution

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-
-import csv 
+import csv
 import json
 
-def convert_csv_to_json(filename):
-    with open(filename, 'a+', newline='') as newfilecsv:
-            csv_reader = csv.DictReader(newfilecsv)
-            json_data = json.dumps(list(csv_reader), indent=4)
-            
-    with open('data.json', 'w') as json_file:
-        json_file.write(json_data)
+"""Convert csv to json"""
+
+def convert_csv_to_json(csv_filename):
+        """Convert csv to json"""
+        with open(csv_filename, 'r', newline='') as f:
+            csv_reader = csv.DictReader(f)
+            data = list(csv_reader)
+
         
-    return True
-                
+        json_data = json.dumps(data, indent=4)
+
+        
+        with open('data.json', 'w') as f_json:
+            f_json.write(json_data)
+
+        return True
