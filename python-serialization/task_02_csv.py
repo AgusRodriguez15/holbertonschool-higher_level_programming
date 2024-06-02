5b9b908442155f0bde02a187006936f5bacf3be4
AgusRodriguez15 <8670@holbertonstudents.com>
2024-06-02 08:36:58 -0800

    task 02 correctition add try to manege if not exists file

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 import csv
 import json
-
 """Convert csv to json"""
 
 
 def convert_csv_to_json(csv_filename):
     """Convert csv to json"""
-    with open(csv_filename, 'r', newline='') as f:
-        csv_reader = csv.DictReader(f)
-        data = list(csv_reader)
+    try:
+        with open(csv_filename, 'r', newline='') as f:
+            csv_reader = csv.DictReader(f)
+            data = list(csv_reader)
 
-    json_data = json.dumps(data, indent=4)
+        json_data = json.dumps(data, indent=4)
 
-    with open('data.json', 'w') as f_json:
-        f_json.write(json_data)
+        with open('data.json', 'w') as f_json:
+            f_json.write(json_data)
 
-    return True
+        return True
+    except FileNotFoundError:
+        return False
