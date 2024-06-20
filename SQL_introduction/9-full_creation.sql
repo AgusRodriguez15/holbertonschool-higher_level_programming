c89fa69eb5a1c0ce27bb3840d6111eddd196ad33
AgusRodriguez15 <8670@holbertonstudents.com>
2024-06-20 02:27:11 +0000

    change name in instert table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS second_table (
     score INT
 );
 
-INSERT INTO first_table(id, name, score) 
+INSERT INTO second_table (id, name, score) 
 VALUES (1, "John", 10),
 VALUES (2, "Alex", 3),
 VALUES (3, "Bob", 14),
