254944f10134ee3bcc59d8b25f70b517b18a4811
AgusRodriguez15 <8670@holbertonstudents.com>
2024-06-21 23:27:43 +0000

    another try

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- create user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-SET PASSWORD FOR 'user_0d_1'@'localhost' = PASSWORD('user_0d_1_pwd');
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON hbtn_0c_0.* TO 'user_0d_1_'@'localhost';
+FLUSH PRIVILEGES;
