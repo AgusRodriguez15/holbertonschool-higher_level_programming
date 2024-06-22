db931c7356b3dd3d7798c85d2b69e0fcef320b80
AgusRodriguez15 <8670@holbertonstudents.com>
2024-06-22 20:52:58 +0000

    fix

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- cities of california
 USE hbtn_0d_usa;
-SELECT name
-    FROM states
-    WHERE name = 'California'
-    ORDER BY cities.id ASC;
+SELECT id, name
+    FROM cities
+    WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+    ORDER BY id ASC;
