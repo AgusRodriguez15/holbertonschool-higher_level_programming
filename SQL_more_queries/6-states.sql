a2470f1bdf81cdd59edaf2b2ff3950babbeb20bd
AgusRodriguez15 <8670@holbertonstudents.com>
2024-06-22 17:21:14 +0000

    fix some problems and add use hbtn_0d_usa

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
 -- create databse and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa; 
 CREATE TABLE IF NOT EXISTS states (
-    id NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
