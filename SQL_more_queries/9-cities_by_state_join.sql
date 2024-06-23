e2662ce20fcc001c2e338ffc447053ae984c79d5
AgusRodriguez15 <8670@holbertonstudents.com>
2024-06-23 23:49:49 +0000

    now

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,6 +2,6 @@
 USE hbtn_0d_usa;
 SELECT cities.id, cities.name, states.name
 FROM cities
-    JOIN states
-        ON cities.state_id = state_id
-ORDER BY cities.id
+    INNER JOIN states
+        ON cities.state_id = states_id
+ORDER BY cities.id;
