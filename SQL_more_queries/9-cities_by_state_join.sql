8a3fb960dd09e638d2f8516bdedac08f3dde2dbf
AgusRodriguez15 <8670@holbertonstudents.com>
2024-06-23 15:12:39 +0000

    change order

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,4 @@
 -- cities by state
-SELECT states.id AS state_id, states.name AS state_name, 
-       cities.id AS city_id, cities.name AS city_name
-FROM states
-INNER JOIN cities ON cities.state_id = state_id;
+SELECT states.id, states.name, cities.id, cities.name FROM cities
+INNER JOIN states ON cities.state_id = state_id;
 ORDER BY cities.id ASC;
