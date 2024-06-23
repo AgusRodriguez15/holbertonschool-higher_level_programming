2c86f0ba176a6230a65bd272508e2ff791e6a86a
AgusRodriguez15 <8670@holbertonstudents.com>
2024-06-23 15:23:56 +0000

    NOW

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- cities by state
 SELECT cities.id, cities.name, states.name FROM cities
         INNER JOIN states ON cities.state_id = state.id
-        ORDER BY cities.id;
+        ORDER BY cities.id
