e73dd4705fe118fa7aec61d30fac0451de5151e8
AgusRodriguez15 <8670@holbertonstudents.com>
2024-07-04 23:45:39 -0300

    corrrect pep8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,6 +3,7 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """class"""
     def area(self):
@@ -15,7 +16,7 @@ class Rectangle(BaseGeometry):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-        
+
     def __init__(self, width, height):
         """inicialitate private"""
         self.integer_validator("width", width)
