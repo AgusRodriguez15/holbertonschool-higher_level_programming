bc5cbb6ebd956dad3690ca1b0004138655f6672b
AgusRodriguez15 <8670@holbertonstudents.com>
2024-07-04 21:51:31 -0300

    raise exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""create an empty class"""
+
+
+class BaseGeometry:
+    """class"""
+    def area(self):
+        """use exception"""
+        raise Exception("area() is not implemented")
