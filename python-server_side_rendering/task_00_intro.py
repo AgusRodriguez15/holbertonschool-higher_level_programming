985f3fee4c172b489d4e374042b2bf1ee425473d
AgusRodriguez15 <8670@holbertonstudents.com>
2024-07-19 23:29:50 -0300

    fixed

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -19,13 +19,14 @@ def generate_invitations(template, attendees):
     with open('template.txt','r') as file:
         text = " ".join(line.rstrip() for line in file)
 
- 
-    for attendee in attendees:
-        replace = text.format(
-            name=attendee.get("name", "N/A"),
-            event_title=attendee.get("event_title", "N/A"),
-            event_date=attendee.get("event_date", "N/A") if attendee["event_date"] else "N/A",
-            event_location=attendee.get("event_location", "N/A")
-        )
+    with open('output_1.txt', 'w') as output_file:
+        for attendee in attendees:
+            replace = text.format(
+                name=attendee.get("name", "N/A"),
+                event_title=attendee.get("event_title", "N/A"),
+                event_date=attendee.get("event_date", "N/A") if attendee["event_date"] else "N/A",
+                event_location=attendee.get("event_location", "N/A")
+            )
+            output_file.write(replace + "\n")
     print(replace)
     
