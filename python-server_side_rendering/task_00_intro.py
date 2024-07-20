e27a2c3720e8bc03d26ef9a9c47d4415cd64f426
AgusRodriguez15 <8670@holbertonstudents.com>
2024-07-20 00:09:20 -0300

    ok

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import os
 
 def generate_invitations (template, attendees):
     
@@ -29,5 +28,3 @@ def generate_invitations (template, attendees):
             )
             output_file.write(replace + "\n")
     print(replace)
-
-    assert os.path.exists('output_2.txt'), "Failed: Output file 2 should be generated"
