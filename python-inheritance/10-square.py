e1caba88682056f9ce939f818f58122f5625034c
AgusRodriguez15 <8670@holbertonstudents.com>
2024-07-29 14:54:55 -0300

    c=Orrect pep8

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,18 +4,17 @@
 
 BaseGeometry = __import__('9-rectangle').BaseGeometry
 
+
 class Square(BaseGeometry):
     """class"""
     def area(self):
         """use exception"""
         return self._size * self._size
-    
+
     def __init__(self, size):
         """inicialitate private"""
         self.integer_validator("size", size)
         self._size = size
-        
+
     def __str__(self):
         return f"[Rectangle] {self._size}/{self._size}"
-    
-
