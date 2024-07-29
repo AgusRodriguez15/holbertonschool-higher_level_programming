21e13d3f1e0f70bea9978b30bfdb0b45bf5c39c6
AgusRodriguez15 <8670@holbertonstudents.com>
2024-07-29 15:00:38 -0300

    the same

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3}
+
+"""create an empty class"""
+
+BaseGeometry = __import__('9-rectangle').Rectangle
+
+
+class Square(BaseGeometry):
+    """class"""
+    def area(self):
+        """use exception"""
+        return self._size * self._size
+
+    def __init__(self, size):
+        """inicialitate private"""
+        self.integer_validator("size", size)
+        self._size = size
+
+    def __str__(self):
+        return f"[Rectangle] {self._size}/{self._size}"
