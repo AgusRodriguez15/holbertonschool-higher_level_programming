fbd50eb5a4d487cdaa9e9486fcd86a6a08b30b86
AgusRodriguez15 <8670@holbertonstudents.com>
2024-07-30 15:53:03 -0300

    solution 1

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+    
+    def perimeter(self):
+        pass
+    
+class Circle(Shape):
+    
+    def __init__(self, radius=0):
+        """def init """
+        self.radius = radius
+    
+    def area(self):
+        return 3.14 * self.radius ** 2
+    
+    def perimeter(self):
+        return 3.14 * self.radius * 2
+    
+class Rectangle(Shape):
+    
+    def __init__(self, width=0, height=0):
+        """def init """
+        self.height = height
+        self.width = width
+        
+    
+    def area(self):
+        return self.width + self.height + self.width + self.height
+    
+    def perimeter(self):
+        return self.width * self.height
+    
+def shape_info(shape):
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
